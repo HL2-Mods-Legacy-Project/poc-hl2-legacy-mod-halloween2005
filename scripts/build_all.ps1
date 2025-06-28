aa297ca2b8ceaf163913a3a5a06c5758713a775d
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-06-28 13:55:58 -0400

    Create build_all.ps1

---
+++ scripts/build_all.ps1
@@ -0,0 +1,8 @@
+& "$PSScriptRoot\..\mod-installer\build_all.ps1" `
+  -Version "1.0.0" `
+  -OutputFilePrefix "Halloween" `
+  -ModName "Halloween HL2: Pumpkin Night (2005)" `
+  -ModFilesPath "$PSScriptRoot\..\game\Halloween" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/poc-hl2-legacy-registry/releases/download/halloween-2005/Halloween-Original.zip" `
+  -ModBaseFilesUrlHash "087fc90281b3a9dc034769a8097a608c6bfd604c3d490ab69c69c8dbb3ae700f" `
+  -PublishPath "$PSScriptRoot\..\artifacts\publish"
